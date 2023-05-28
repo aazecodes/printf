7afafa6a9b59bec1bbe3012468bc4581225a1a45
aazecodes <abdussalamsafiyah@gmail.com>
2023-05-28 14:51:07 -0700

    format_output

--- format_output.c
+++ format_output.c
@@ -1,7 +1,4 @@
 #include "main.h"
-
-void handle_buffers(char buffer[], int *buff_ind);
-
 /**
  * _printf - Printf function
  * @format: format.
@@ -9,58 +6,51 @@ void handle_buffers(char buffer[], int *buff_ind);
  */
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
+	int i = 0, c;
+	char *s;
+	va_list args;
 
-	if (format == NULL)
-		return (-1);
+	va_start(args, format);
 
-	va_start(list, format);
-
-	for (i = 0; format && format[i] != '\0'; i++)
+	while (*format != '\0')
 	{
-		if (format[i] != '%')
+		if (*format == '%')
 		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				handle_buffers(buffer, &buff_ind);
-			printed_chars++;
+			format++;
+
+			if (*format == '\0')
+				break;
+
+			if (*format == '%')
+			{
+				putchar('%');
+				i++;
+			}
+			else if (*format == 'c')
+			{
+				c = va_arg(args, int);
+				putchar(c);
+				i++;
+			}
+			else if (*format == 's')
+			{
+				s = va_arg(args, char *);
+				while (*s != '\0')
+				{
+					putchar(*s);
+					i++;
+					s++;
+				}
+			}
 		}
 		else
 		{
-			handle_buffers(buffer, &buff_ind);
-			flags = fetch_flags(format, &i);
-			width = fetch_width(format, &i, list);
-			precision = fetch_precision(format, &i, list);
-			size = fetch_size(format, &i);
-			++i;
-			printed = run_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
+			putchar(*format);
+			i++;
 		}
+
+		format++;
 	}
-
-	handle_buffers(buffer, &buff_ind);
-
-	va_end(list);
-
-	return (printed_chars);
+	va_end(args);
+	return (i);
 }
-
-/**
- * handle_buffers - Prints buffer contents if exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void handle_buffers(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
-}
-
