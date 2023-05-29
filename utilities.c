66eb72e8d75889a7dcd957fd1e5c6741fdeca6fb
aazecodes <abdussalamsafiyah@gmail.com>
2023-05-29 13:58:10 -0700

    checking through

--- utilities.c
+++ utilities.c
@@ -3,8 +3,7 @@
 /**
  * is_printable - Evaluates if a char is printable
  * @c: Char to be evaluated.
- *
- * Return: 1 if c is printable, 0 otherwise
+ * Return: 1 or 0
  */
 int is_printable(char c)
 {
@@ -19,29 +18,25 @@ int is_printable(char c)
  * @buffer: Array of chars.
  * @i: Index at which to start appending.
  * @ascii_code: ASSCI CODE.
- * Return: Always 3
+ * Return: success
  */
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
 	char map_to[] = "0123456789ABCDEF";
-	/* The hexa format code is always 2 digits long */
+
 	if (ascii_code < 0)
 		ascii_code *= -1;
-
 	buffer[i++] = '\\';
 	buffer[i++] = 'x';
-
 	buffer[i++] = map_to[ascii_code / 16];
 	buffer[i] = map_to[ascii_code % 16];
-
 	return (3);
 }
 
 /**
  * is_digit - Verifies if a char is a digit
  * @c: Char to be evaluated
- *
- * Return: 1 if c is a digit, 0 otherwise
+ * Return: 1 or 0
  */
 int is_digit(char c)
 {
@@ -84,4 +79,3 @@ long int convert_size_unsgnd(unsigned long int num, int size)
 
 	return ((unsigned int)num);
 }
-
